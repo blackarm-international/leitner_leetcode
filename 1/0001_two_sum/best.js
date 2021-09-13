d4e46fedc9cb26af1d30ba61abd535b54551d3e4
blackarmVR <magnusmichaelson@protonmail.com>
2021-09-13 19:34:50 +0100

    :)

--- 1/0001_two_sum/best.js
+++ 1/0001_two_sum/best.js
@@ -3,19 +3,11 @@ let target;
 var twoSum = function(nums, target) {
     var hash = {};
     var len = nums.length;
-    // console.log('nums - ' + JSON.stringify(nums));
-    // console.log('target - ' + JSON.stringify(target));
     for (var i = 0; i < len; i++) {
-        // console.log('');
-        // console.log('value - ' + nums[1]);
-        // console.log('position - ' + i);
-        // does the hash have this number as a key
         if (nums[i] in hash) {
             return [hash[nums[i]], i];
         }
-        // store this in an object where the key is (target - number) and the value is the position in the array
         hash[target - nums[i]] = i;
-        // console.log(hash);
     }
     return [-1, -1];
 };
