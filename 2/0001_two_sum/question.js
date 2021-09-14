e4c50ebeeaa7bbc37c6b1d1f7dcb5c3b37b9ef9f
blackarmVR <honungsbjorn@protonmail.com>
2021-09-14 12:38:16 +0100

    :)

---
+++ 2/0001_two_sum/question.js
@@ -0,0 +1,19 @@
+
+let nums = [];
+let target;
+
+nums = [2,7,11,15];
+target = 9;
+
+// nums = [3,2,4];
+// target = 6;
+
+// nums = [3,3];
+// target = 6;
+
+
+var twoSum = function(nums, target) {
+    return [-1, -1];
+};
+
+console.log(twoSum(nums, target))
